23ee8a279ea5b266507e13302b05e9109ac1b798
15980904537 <2565051131@qq.com>
2021-06-29 12:19:59 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, HashRouter} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import {Home} from './pages/home';
+import {Config} from './pages/config';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/config">
+            <Config/>
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './home.scss'
+
+export const Home = () => {
+  return (
+    <div>
+      <div className="x"></div>
+    </div>
+  )
+}
