10f7a1732fe0d7797f0256c02ce43f79ff76efa1
15980904537 <2565051131@qq.com>
2021-06-29 13:33:30 +0800

    构建页面的整体布局（使用flex，grid）

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,15 @@ import imgageHeader from '../images/header.png'
 
 export const Home = () => {
   return (
-    <div>
+    <div className="home">
       <header style={{ backgroundImage: `url(${imgageHeader})` }}></header>
+      <main>
+        <section className="section1">1</section>
+        <section className="section2">2</section>
+        <section className="section3">3</section>
+        <section className="section4">4</section>
+        <section className="section5">5</section>
+      </main>
     </div>
   )
 }
